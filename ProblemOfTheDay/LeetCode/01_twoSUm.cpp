7b73bb5355241772ecc96792a9b56c3c3831d8f1
ArjunPathania <arjunpathania786f@gmail.com>
2024-10-06 00:43:17 +0530

    two sum problem 05/10/2024

---
+++ ProblemOfTheDay/LeetCode/01_twoSUm.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Solution class
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        // brute force approach
+        // vector<int> ans;
+        // for (int i = 0; i < nums.size(); i++)
+        // {
+        //     for (int j = i + 1; j < nums.size(); j++)
+        //     {
+        //         if ((nums[i] + nums[j]) == target)
+        //         {
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //             return ans;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // using a hashMap
+        unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end())
+            {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main()
+{
+    // Fast IO
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T;
+    /* cout << "Enter number of test cases: ";*/
+    cin >> T;
+
+    while (T--)
+    {
+        int N;
+        if (!(cin >> N))
+        {
+            cerr << "Insufficient inputs for the number of test cases specified." << endl;
+            return 1; // Exit with error code
+        }
+        int target;
+        cin >> target;
+        vector<int> nums(N);
+        for (int i = 0; i < N; i++)
+        {
+            cin >> nums[i];
+        }
+        Solution obj;
+        vector<int> ans = obj.twoSum(nums, target);
+        for (int i = 0; i < 2; i++)
+        {
+            cout << ans[i] << " ";
+        }
+    }
+
+    return 0;
+}
